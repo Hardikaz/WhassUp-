86cac4c1ce983f40ac342afc105d4d4c9ae75fba
Hardik Sharma <sharmahardikaz@gmail.com>
2024-08-27 15:32:44 +0530

    Client side set up done layout created

---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+
+function App() {
+  return (
+   <main>
+    <header>
+      <a href="" className="logo">MyBlog</a>
+      <nav>
+        <a href="">Login</a>
+        <a href="">Register</a>
+      </nav>
+    </header>
+
+    <div className="post">
+    <div className='image'>
+    <img src='https://media.istockphoto.com/id/843408508/photo/photography-camera-lens-concept.jpg?s=612x612&w=0&k=20&c=-tm5TKrPDMakrT1vcOE-4Rlyj-iBVdzKuX4viFkd7Vo='/>
+    </div>
+    <div className="text">
+    <h2>Full house battery coming later this year</h2>
+    <p>Lorem ipsum sit amet Full house battery coming later this year</p>
+    </div>
+    </div>
+
+    <div className="post">
+    <div className='image'>
+    <img src='https://media.istockphoto.com/id/843408508/photo/photography-camera-lens-concept.jpg?s=612x612&w=0&k=20&c=-tm5TKrPDMakrT1vcOE-4Rlyj-iBVdzKuX4viFkd7Vo='/>
+    </div>
+    <div className="text">
+    <h2>Full house battery coming later this year</h2>
+    <p>Lorem ipsum sit amet Full house battery coming later this year</p>
+    </div>
+    </div>
+
+    <div className="post">
+    <div className='image'>
+    <img src='https://media.istockphoto.com/id/843408508/photo/photography-camera-lens-concept.jpg?s=612x612&w=0&k=20&c=-tm5TKrPDMakrT1vcOE-4Rlyj-iBVdzKuX4viFkd7Vo='/>
+    </div>
+    <div className="text">
+    <h2>Full house battery coming later this year</h2>
+    <p>Lorem ipsum sit amet Full house battery coming later this year</p>
+    </div>
+    </div>
+
+    <div className="post">
+    <div className='image'>
+    <img src='https://media.istockphoto.com/id/843408508/photo/photography-camera-lens-concept.jpg?s=612x612&w=0&k=20&c=-tm5TKrPDMakrT1vcOE-4Rlyj-iBVdzKuX4viFkd7Vo='/>
+    </div>
+    <div className="text">
+    <h2>Full house battery coming later this year</h2>
+    <p>Lorem ipsum sit amet Full house battery coming later this year</p>
+    </div>
+    </div>
+
+   </main>
+  );
+}
+
+export default App;
