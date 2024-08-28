a463bc06f80ac72844d2a20881c76ce582fe63fb
Hardik Sharma <sharmahardikaz@gmail.com>
2024-08-28 23:41:10 +0530

    New post upload functionality added

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ const cookieParser=require('cookie-parser');
 const multer=require('multer')
 const uploadMiddleware=multer({dest: 'uploads/'});
 const fs=require('fs');
+const Post=require('./models/Post');
 const app=express();
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
@@ -75,13 +76,22 @@ app.post('/logout',(req,res)=>{
     res.cookie('token','').json('ok');
 })
 
-app.post('/post',uploadMiddleware.single('file'),(req,res)=>{
+app.post('/post',uploadMiddleware.single('file'),async (req,res)=>{
     const {originalname,path}=req.file;
     const parts=originalname.split('.');
     const ext=parts[parts.length-1];
     const newPath=path+'.'+ext;
-    fs.renameSync(newPath);
-    res.json({ext});
+    fs.renameSync(path,newPath);
+
+    const {title,summary,content}=req.body;
+    const postDoc=await Post.create({
+        title,
+        summary,
+        content,
+        cover:newPath,
+    });
+
+    res.json(postDoc);
 })
 
 
